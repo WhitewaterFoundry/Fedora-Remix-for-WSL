113245cc648a0963d8934b750e43bb48783645e1
Hayden <hayden@whitewaterfoundry.com>
2018-11-21 13:26:20 -0500

    Update

--- create-targz-x64.sh
+++ create-targz-x64.sh
@@ -6,20 +6,20 @@ yum_config=/etc/yum.conf
 set -x
 
 mkdir -m 755 "$target"/dev
-mknod -m 600 "$target"/dev/console c 5 1
-mknod -m 600 "$target"/dev/initctl p
-mknod -m 666 "$target"/dev/full c 1 7
-mknod -m 666 "$target"/dev/null c 1 3
-mknod -m 666 "$target"/dev/ptmx c 5 2
-mknod -m 666 "$target"/dev/random c 1 8
-mknod -m 666 "$target"/dev/tty c 5 0
-mknod -m 666 "$target"/dev/tty0 c 4 0
-mknod -m 666 "$target"/dev/urandom c 1 9
-mknod -m 666 "$target"/dev/zero c 1 5
+sudo mknod -m 600 "$target"/dev/console c 5 1
+sudo mknod -m 600 "$target"/dev/initctl p
+sudo mknod -m 666 "$target"/dev/full c 1 7
+sudo mknod -m 666 "$target"/dev/null c 1 3
+sudo mknod -m 666 "$target"/dev/ptmx c 5 2
+sudo mknod -m 666 "$target"/dev/random c 1 8
+sudo mknod -m 666 "$target"/dev/tty c 5 0
+sudo mknod -m 666 "$target"/dev/tty0 c 4 0
+sudo mknod -m 666 "$target"/dev/urandom c 1 9
+sudo mknod -m 666 "$target"/dev/zero c 1 5
 
-yum -c "$yum_config" --installroot="$target" --releasever=/ --setopt=tsflags=nodocs --setopt=group_package_types=mandatory -y groupinstall "Core"
+yum -c "$yum_config" --installroot="$target" --releasever=/ groupinstall "Core"
 
-yum -c "$yum_config" --installroot="$target" --releasever=/ --setopt=tsflags=nodocs --setopt=group_package_types=mandatory -y install "git curl python3"
+yum -c "$yum_config" --installroot="$target" --releasever=/ install "git curl python3"
 
 yum -c "$yum_config" --installroot="$target" -y clean all
 
@@ -34,4 +34,4 @@ mkdir -p --mode=0755 "$target"/var/cache/yum
 rm -rf "$target"/etc/ld.so.cache "$target"/var/cache/ldconfig
 mkdir -p --mode=0755 "$target"/var/cache/ldconfig
 
-tar --ignore-failed-read --numeric-owner -czvf $TMPDIR/install.tar.gz *
+tar --ignore-failed-read --numeric-owner -czvf install.tar.gz $target/*
