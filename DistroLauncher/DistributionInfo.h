fa99cf9a3fbaca5b959ed4a435163d1361b153b3
Hayden <hayden@whitewaterfoundry.com>
2018-11-23 13:12:40 -0500

    Update

--- DistroLauncher/DistributionInfo.h
+++ DistroLauncher/DistributionInfo.h
@@ -13,7 +13,7 @@ namespace DistributionInfo
     //
     // WARNING: This value must not change between versions of your app,
     // otherwise users upgrading from older versions will see launch failures.
-    const std::wstring Name = L"WLinux Enterprise";
+    const std::wstring Name = L"WLE";
 
     // The title bar for the console window while the distribution is installing.
     const std::wstring WindowTitle = L"WLinux Enterprise";
