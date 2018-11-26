7e7de1c02e9a4505f0cb71fb603ede0427d1fc63
Hayden <hayden@whitewaterfoundry.com>
2018-11-26 13:58:45 -0500

    Update

--- create-targz.sh
+++ create-targz.sh
@@ -1,54 +1,61 @@
 set -e
 
+#declare variables
 ORIGINDIR=$(pwd)
 TMPDIR=$(mktemp -d)
 BUILDDIR=$(mktemp -d)
 
+#enterprise boot ISO
 BOOTISO="http://mirror.centos.org/centos/7.5.1804/os/x86_64/images/boot.iso"
+
+#enterprise Docker kickstart file
 KSFILE="https://raw.githubusercontent.com/CentOS/sig-cloud-instance-build/master/docker/centos-7.ks"
 
 # ARM64
 # BOOTISO="http://vault.centos.org/altarch/7.3.1611/os/aarch64/images/boot.iso"
 # KSFILE="https://raw.githubusercontent.com/CentOS/sig-cloud-instance-build/master/docker/centos-7arm64.ks"
 
-# BOOTISO="http://fedora.mirror.constant.com/fedora/linux/releases/29/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-29-1.2.iso"
-# KSFILE="https://pagure.io/fedora-kickstarts/raw/master/f/fedora-docker-base.ks"
-
-# Download ISO. Comment out line 37 and edit --iso in line 33 to point to your downloaded ISO.
-# KSFILE="https://gist.githubusercontent.com/bargenson/24e9c4883a4adbcfbfd4/raw/3c1aae56a6565c51ec674d61d041123134bab6c6/docker-oracle-linux.ks"
-
-# Download ISO. Comment out line 37 and edit --iso in line 33 to point to your downloaded ISO.
-# KSFILE="https://raw.githubusercontent.com/CentOS/sig-cloud-instance-build/master/docker/centos-7.ks"
-
+#go to our temporary directory
 cd $TMPDIR
 
+#make sure we are up to date
 sudo yum update
 
+#get livemedia-creator dependencies
 sudo yum install libvirt lorax virt-install libvirt-daemon-config-network libvirt-daemon-kvm libvirt-daemon-driver-qemu
 
+#restart libvirtd for good measure
 sudo systemctl restart libvirtd
 
+#download enterprise boot ISO
 sudo curl $BOOTISO -o /tmp/install.iso
 
+#download enterprise Docker kickstart file
 curl $KSFILE -o install.ks
 
+#build intermediary rootfs tar
 sudo livemedia-creator --make-tar --iso=/tmp/install.iso --image-name=install.tar.xz --ks=install.ks --releasever "7"
 
+#open up the tar into our build directory
 tar -xvf /var/tmp/install.tar.xz -C $BUILDDIR
 
+#copy some custom files into our build directory 
 sudo cp $ORIGINDIR/linux_files/wsl.conf $BUILDDIR/etc/wsl.conf
 sudo cp $ORIGINDIR/linux_files/local.conf $BUILDDIR/etc/local.conf
-#sudo cp $ORIGINDIR/linux_files/wslu.yum.conf $BUILDDIR/etc/yum.conf.d/wslu.yum.conf
 
+#set some environmental variables in our build directory
 sudo bash -c "echo 'export DISPLAY=:0' >> $BUILDDIR/etc/profile"
 sudo bash -c "echo 'export LIBGL_ALWAYS_INDIRECT=1' >> $BUILDDIR/etc/profile"
 sudo bash -c "echo 'export NO_AT_BRIDGE=1' >> $BUILDDIR/etc/profile"
 
+#re-build our tar image
 cd $BUILDDIR
 tar --ignore-failed-read -czvf $ORIGINDIR/install.tar.gz *
 
+#go home
 cd $ORIGINDIR
 
+#clean up
 sudo rm -r $BUILDDIR
 sudo rm -r $TMPDIR
 sudo rm /tmp/install.iso
