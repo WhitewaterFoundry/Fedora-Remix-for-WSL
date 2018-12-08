1ed231da023d91031ffe37ae3bd972f8f70eae3a
Kim Bradley <kim.jamie.bradley@gmail.com>
2018-12-08 09:24:37 +0000

    Remove unused code + add comments
    Signed-off-by: Kim Bradley <kim.jamie.bradley@gmail.com>

--- create-targz.sh
+++ create-targz.sh
@@ -1,41 +1,32 @@
 #!/bin/bash
 
+# Set environment
 set -e
 ORIGINDIR=$(pwd)
 TMPDIR=$(mktemp -d)
 ARCH="x64"
 
-set -x
-
+# Move to our temporary directory
 cd $TMPDIR
 mkdir $TMPDIR/dist
 
+# Make sure /dev is created before later mount
 mkdir -m 0755 $TMPDIR/dist/dev
-#mount --bind /dev $TMPDIR/dist/dev
-#mock --init --rootdir=$TMPDIR/dist
+
+# Use mock to initialise chroot filesystem
 mock --init --dnf --rootdir=$TMPDIR/dist
+
+# Bind mount current /dev to new chroot/dev
+# (fixes '/dev/null: Permission denied' errors)
 mount --bind /dev $TMPDIR/dist/dev
+
+# Install required packages, autoremove then clean
 dnf --installroot=$TMPDIR/dist --releasever=/ -y groupinstall core --exclude=grub\*
 dnf --installroot=$TMPDIR/dist --releasever=/ -y autoremove
 dnf --installroot=$TMPDIR/dist --releasever=/ -y clean all
-umount $TMPDIR/dist/dev
-#mock --dnf --install coreutils
-#mkdir -m 755 $CHROOTDIR/dev/
-#mknod -m 600 $CHROOTDIR/dev/console c 5 1
-#mknod -m 600 $CHROOTDIR/dev/initctl p
-#mknod -m 666 $CHROOTDIR/dev/full c 1 7
-#mknod -m 666 $CHROOTDIR/dev/null c 1 3
-#mknod -m 666 $CHROOTDIR/dev/ptmx c 5 2
-#mknod -m 666 $CHROOTDIR/dev/random c 1 8
-#mknod -m 666 $CHROOTDIR/dev/tty c 5 0
-#mknod -m 666 $CHROOTDIR/dev/tty0 c 4 0
-#mknod -m 666 $CHROOTDIR/dev/urandom c 1 0
-#mknod -m 666 $CHROOTDIR/dev/zero c 1 5
 
-#dnf --installroot=$CHROOTDIR --releasever=/ -y install coreutils
-#dnf -c /etc/dnf/dnf.conf --installroot=$CHROOTDIR --releasever=/ --setopt=group_package_types=mandatory -y groupinstall "Core" --exclude=grub\*
-#dnf -c /etc/dnf/dnf/conf --installroot=$CHROOTDIR -y install passwd sudo dnf
-#dnf -c /etc/dnf/dnf.conf --installroot=$CHROOTDIR -y clean all
+# Unmount /dev
+umount $TMPDIR/dist/dev
 
 #cat > $CHROOTDIR/etc/sysconfig/network <<EOF
 #NETWORKING=yes
@@ -46,6 +37,9 @@ umount $TMPDIR/dist/dev
 cp $ORIGINDIR/linux_files/wsl.conf $TMPDIR/dist/etc/wsl.conf
 cp $ORIGINDIR/linux_files/local.conf $TMPDIR/dist/etc/local.conf
 
+# Create filesystem tar
 cd $TMPDIR/dist
 tar --numeric-owner -czvf $ORIGINDIR/$ARCH/install.tar.gz *
+
+# Cleanup
 rm -rf $TMPDIR
