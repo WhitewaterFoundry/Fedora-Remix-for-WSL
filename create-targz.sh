7398a806de1a608a607c6e8ed79c0b070fd16688
Kim Bradley <grufwub@fedora-7390.Home>
2018-12-08 00:01:24 +0000

    Changes to create-targz.sh
    Signed-off-by: Kim Bradley <grufwub@fedora-7390.Home>

--- create-targz.sh
+++ create-targz.sh
@@ -1,63 +1,49 @@
 #!/bin/bash
 
 set -e
-BUILDDIR=$(pwd)
+ORIGINDIR=$(pwd)
 TMPDIR=$(mktemp -d)
-ARCH="amd64"
-cd $TMPDIR
+ARCH="x64"
 
 set -x
 
-mkdir -m 755 $TMPDIR/dev/
-mknod -m 600 $TMPDIR/dev/console c 5 1
-mknod -m 600 $TMPDIR/dev/initctl p
-mknod -m 666 $TMPDIR/dev/full c 1 7
-mknod -m 666 $TMPDIR/dev/null c 1 3
-mknod -m 666 $TMPDIR/dev/ptmx c 5 2
-mknod -m 666 $TMPDIR/dev/random c 1 8
-mknod -m 666 $TMPDIR/dev/tty c 5 0
-mknod -m 666 $TMPDIR/dev/tty0 c 4 0
-mknod -m 666 $TMPDIR/dev/urandom c 1 0
-mknod -m 666 $TMPDIR/dev/zero c 1 5
+cd $TMPDIR
+mkdir $TMPDIR/dist
 
-# Set variables
-if [ -d /etc/dnf/vars ] ; then
-    mkdir -p -m 755 $TMPDIR/etc/dnf
-    cp -ar /etc/dnf/vars $TMPDIR/etc/dnf/vars
-fi
+mkdir -m 0755 $TMPDIR/dist/dev
+#mount --bind /dev $TMPDIR/dist/dev
+#mock --init --rootdir=$TMPDIR/dist
+mock --init --dnf --rootdir=$TMPDIR/dist
+mount --bind /dev $TMPDIR/dist/dev
+dnf --installroot=$TMPDIR/dist --releasever=/ -y groupinstall core --exclude=grub\*
+umount $TMPDIR/dist/dev
+#mock --dnf --install coreutils
+#mkdir -m 755 $CHROOTDIR/dev/
+#mknod -m 600 $CHROOTDIR/dev/console c 5 1
+#mknod -m 600 $CHROOTDIR/dev/initctl p
+#mknod -m 666 $CHROOTDIR/dev/full c 1 7
+#mknod -m 666 $CHROOTDIR/dev/null c 1 3
+#mknod -m 666 $CHROOTDIR/dev/ptmx c 5 2
+#mknod -m 666 $CHROOTDIR/dev/random c 1 8
+#mknod -m 666 $CHROOTDIR/dev/tty c 5 0
+#mknod -m 666 $CHROOTDIR/dev/tty0 c 4 0
+#mknod -m 666 $CHROOTDIR/dev/urandom c 1 0
+#mknod -m 666 $CHROOTDIR/dev/zero c 1 5
 
-dnf -c /etc/dnf/dnf.conf --installroot=$TMPDIR --releasever=/ --setopt=tsflags=nodocs --setopt=group_package_types=mandatory -y groupinstall "Core"
-dnf -c /etc/dnf/dnf.conf --installroot=$TMPDIR -y clean all
+#dnf --installroot=$CHROOTDIR --releasever=/ -y install coreutils
+#dnf -c /etc/dnf/dnf.conf --installroot=$CHROOTDIR --releasever=/ --setopt=group_package_types=mandatory -y groupinstall "Core" --exclude=grub\*
+#dnf -c /etc/dnf/dnf/conf --installroot=$CHROOTDIR -y install passwd sudo dnf
+#dnf -c /etc/dnf/dnf.conf --installroot=$CHROOTDIR -y clean all
 
-cat > $TMPDIR/etc/sysconfig/network <<EOF
-NETWORKING=yes
-HOSTNAME=localhost.localdomain
-EOF
-
-# Remove extra locales
-rm -rf $TMPDIR/usr/{{lib,share}/locale,{lib,lib64}/gconv,bin/localedef,sbin/build-locale-archive}
-# Remove extra docs / man pages
-rm -rf $TMPDIR/usr/share/{man,doc,info,gnome/help}
-# Remove cracklib
-rm -rf $TMPDIR/usr/share/cracklib
-# Remove i18n
-rm -rf $TMPDIR/usr/share/i18n
-# Remove yum / dnf cache
-if [ -d $TMPDIR/var/cache/yum ] ; then
-    rm -rf $TMPDIR/var/cache/yum
-    mkdir -p --mode=0755 $TMPDIR/var/cache/yum
-fi
-rm -rf $TMPDIR/var/cache/dnf
-mkdir -p --mode=0755 $TMPDIR/var/cache/dnf
-# Remove sln
-rm -rf $TMPDIR/sbin/sln
-# Remove ldconfig
-rm -rf $TMPDIR/etc/ld.so.cache $TMPDIR/var/cache/ldconfig
-mkdir -p --mode=0755 $TMPDIR/var/cache/ldconfig
+#cat > $CHROOTDIR/etc/sysconfig/network <<EOF
+#NETWORKING=yes
+#HOSTNAME=localhost.localdomain
+#EOF
 
 # Copy our own files
-cp $BUILDDIR/linux_files/wsl.conf $TMPDIR/etc/wsl.conf
-mkdir $TMPDIR/etc/fonts
-cp $BUILDDIR/linux_files/local.conf $TMPDIR/etc/fonts/local.conf
+cp $ORIGINDIR/linux_files/wsl.conf $TMPDIR/dist/etc/wsl.conf
+cp $ORIGINDIR/linux_files/local.conf $TMPDIR/dist/etc/local.conf
 
-tar --numeric-owner -czvf $BUILDDIR/install.tar.gz *
+cd $TMPDIR/dist
+tar --numeric-owner -czvf $ORIGINDIR/$ARCH/install.tar.gz *
+rm -rf $TMPDIR
