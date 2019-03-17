c148179b4225b88249522e9e5b499eae30a553c3
Hayden <hayden@whitewaterfoundry.com>
2019-03-16 20:20:59 -0400

    Update create-targz.sh

--- create-targz.sh
+++ create-targz.sh
@@ -35,16 +35,27 @@ umount $TMPDIR/dist/dev
 
 # Copy over some of our custom files
 cp $ORIGINDIR/linux_files/dnf.conf $TMPDIR/dist/etc/dnf/dnf.conf
-cp $ORIGINDIR/linux_files/os-release $TMPDIR/dist/etc/os-release
 cp $ORIGINDIR/linux_files/wsl.conf $TMPDIR/dist/etc/wsl.conf
 cp $ORIGINDIR/linux_files/local.conf $TMPDIR/dist/etc/local.conf
 cp $ORIGINDIR/linux_files/remix.sh $TMPDIR/dist/etc/profile.d/remix.sh
 cp $ORIGINDIR/linux_files/wslutilities.repo $TMPDIR/dist/etc/yum.repos.d/wslutilties.repo
 
-# Add additional necessary packages, comply with Fedora Remix terms, reinstall crypto-policies, remove left over packages, and then clean up
+# Comply with Fedora Remix terms
 systemd-nspawn -q -D $TMPDIR/dist /bin/bash << EOF
 dnf -y update
-dnf -y install cracklib-dicts generic-release wslu --exclude=grub\*,dracut*,grubby,kpartx,kmod,os-prober,libkcapi* --allowerasing
+dnf -y install generic-release --allowerasing
+EOF
+
+# Overwrite os-release provided by generic-release
+cp $ORIGINDIR/linux_files/os-release $TMPDIR/dist/etc/os-release
+
+# Install cracklibs-dicts and wslutilities
+systemd-nspawn -q -D $TMPDIR/dist /bin/bash << EOF
+dnf -y install cracklib-dicts wslu
+EOF
+
+# Reinstall crypto-policies and clean up
+systemd-nspawn -q -D $TMPDIR/dist /bin/bash << EOF
 dnf -y reinstall crypto-policies --exclude=grub\*,dracut*,grubby,kpartx,kmod,os-prober,libkcapi*
 dnf -y autoremove
 dnf -y clean all
