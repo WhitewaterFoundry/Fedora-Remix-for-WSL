3a2af38e7afb020d5725e45ad74ae84dca253134
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2020-04-21 10:53:56 +0200

    Performs an update before everything

--- linux_files/upgrade.sh
+++ linux_files/upgrade.sh
@@ -15,18 +15,17 @@ if [ ${CHANGED} -eq 1 ]; then
 fi
 
 sudo rm -f /etc/yum.repos.d/wslutilties.repo
-(
-  sudo rm -f /var/lib/rpm/.rpm.lock
-  source /etc/os-release && sudo dnf -y copr enable wslutilities/wslu "${ID_LIKE}"-"${VERSION_ID}"-"$(uname -m)"
-)
-sudo rm -f /var/lib/rpm/.rpm.lock
-sudo dnf -y update wslu
-
-# Update the release and main startup script files
-sudo curl -f https://raw.githubusercontent.com/WhitewaterFoundry/Fedora-Remix-for-WSL/master/linux_files/00-remix.sh -o /etc/profile.d/00-remix.sh
-sudo curl -f https://raw.githubusercontent.com/WhitewaterFoundry/Fedora-Remix-for-WSL/master/linux_files/os-release -o /etc/os-release
-
 sudo rm -f /var/lib/rpm/.rpm.lock
 sudo dnf -y update
 sudo rm -f /var/lib/rpm/.rpm.lock
 
+(
+  source /etc/os-release && sudo dnf -y copr enable wslutilities/wslu "${ID_LIKE}"-"${VERSION_ID}"-"$(uname -m)"
+)
+sudo rm -f /var/lib/rpm/.rpm.lock
+sudo dnf -y update wslu
+sudo rm -f /var/lib/rpm/.rpm.lock
+
+# Update the release and main startup script files
+sudo curl -f https://raw.githubusercontent.com/WhitewaterFoundry/Fedora-Remix-for-WSL/master/linux_files/00-remix.sh -o /etc/profile.d/00-remix.sh
+sudo curl -f https://raw.githubusercontent.com/WhitewaterFoundry/Fedora-Remix-for-WSL/master/linux_files/os-release -o /etc/os-release
