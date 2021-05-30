d6518807217bb336b0c093d8d7297d33d807402a
Carlos Ramirez <crramirez@gmail.com>
2021-05-31 01:19:01 +0200

    Register push notifications for Fedora Remix

---
+++ FedoraRemixWSLUI/App.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "App.xaml.g.h"
+
+namespace winrt::FedoraRemixWSLUI::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(const Windows::ApplicationModel::Activation::LaunchActivatedEventArgs&);
+        void OnSuspending(const IInspectable&, const Windows::ApplicationModel::SuspendingEventArgs&);
+        void OnNavigationFailed(const IInspectable&, const Windows::UI::Xaml::Navigation::NavigationFailedEventArgs&);
+        fire_and_forget SetupPushNotifications() const;
+        void OnActivated(const Windows::ApplicationModel::Activation::IActivatedEventArgs&);
+    };
+}
