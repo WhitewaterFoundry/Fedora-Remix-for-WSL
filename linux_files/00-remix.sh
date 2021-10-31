4e1aaa1160ba82b79d0614a0216bee521af11b70
Kairui Song <ryncsn@gmail.com>
2021-10-31 20:01:43 +0800

    Don't call cmd.exe if not needed
    cmd.exe is called in 00-remix.sh to get and set the WIN_HOME variable.
    
    When spawning multiple shell instances inside a already running interactive
    shell, this cmd.exe call slows down the startup speed by a lot, skip this call
    if WIN_HOME is already set.

--- linux_files/00-remix.sh
+++ linux_files/00-remix.sh
@@ -69,7 +69,7 @@ alias clear='clear -x'
 alias ll='ls -al'
 
 # Check if we have Windows Path
-if (command -v cmd.exe >/dev/null 2>&1); then
+if [ -z "$WIN_HOME" ] && (command -v cmd.exe >/dev/null 2>&1); then
 
   # Create a symbolic link to the windows home
 
